e40844a0db3f0ed054796c6744591a0d91785712
Luis Bermudez <lapbermudez@gmai.com>
2022-09-12 16:32:40 -0500

    add css, sass, postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
@@ -6,6 +7,15 @@ module.exports = {
 
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -16,12 +26,13 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "./dist"),
     },
-
     compress: true,
     // port: 8001, // default 8080
   },
